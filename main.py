fcaaad728905607d7f3e1ed1d6dd19347ff160d7
wangzhe <wangzhe199201201@gmail.com>
2020-09-21 10:30:45 +0800

    first time

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from graphics import *
+
+def main():
+    win = GraphWin()
+
+    p = Point(80,80)
+
+    p.draw(win)
+
+
+main()
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
